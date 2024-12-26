790c45019fc5de889501bb85b376006641c36bfa
zhuko <zhukov_ai@outlook.com>
2024-12-26 17:39:03 +0300

    Кто молодец? Я молодец!

--- stone.py
+++ stone.py
@@ -1,9 +1,13 @@
 import random, pygame
 
+
 def made_stone(x_stone, y_stone):
     stone_slovar = {"coord": [x_stone, y_stone], "speed_x": random.randint(-3, 3), "speed_y": random.randint(2, 3),
-                    "angle": random.randint(0, 360), "center_x": 10, "center_y": 10}
+                    "angle": random.randint(0, 360),"coord_draw": [x_stone,y_stone]}
+
     stone_slovar["rotate"] = pygame.transform.rotate(transform_stone, stone_slovar["angle"])
+    size_stone = stone_slovar["rotate"].get_size()
+    stone_slovar["center"] = [stone_slovar["coord"][0] + size_stone[0] / 2, stone_slovar["coord"][1] + size_stone[1] / 2]
 
     while stone_slovar["speed_x"] == 0 or stone_slovar["speed_x"] == -1 or stone_slovar["speed_x"] == 1:
         stone_slovar["speed_x"] = random.randint(-3, 3)
@@ -12,17 +16,27 @@ def made_stone(x_stone, y_stone):
 
 
 def polet_stone(stone):
-    # stone["coord"][0] += stone["speed_x"]
-    # stone["coord"][1] += stone["speed_y"]
+    #TODO: В начале урока запускаешь программу и смотришь на камни - они движутся рывками P.s Вячеслав тоже не знает почему
+    stone["coord"][0] += stone["speed_x"]/10
+    stone["coord"][1] += stone["speed_y"]/10
     stone["rotate"] = pygame.transform.rotate(transform_stone, stone["angle"])
     stone["angle"] += 1
+
+    size_stone = stone["rotate"].get_size()
+    center_rotate = [stone["coord"][0] + size_stone[0] / 2, stone["coord"][1] + size_stone[1] / 2]
+    center_rotate[0] -= stone["center"][0]
+    center_rotate[1] -= stone["center"][1]
+
+    stone["coord_draw"][0] = stone["coord"][0] - center_rotate[0]
+    stone["coord_draw"][1] = stone["coord"][1] - center_rotate[1]
 
 
 def paint(stone, display: pygame.Surface):
     display.blit(stone["rotate"], stone["coord"])
     size_stone = stone["rotate"].get_size()
-    pygame.draw.circle(display,[255,0,0],[stone["coord"][0]+size_stone[0]/2,stone["coord"][1]+size_stone[1]/2],3)
-    pygame.draw.rect(display,[255,255,255],[stone["coord"][0],stone["coord"][1],size_stone[0],size_stone[1]],3)
+    # pygame.draw.circle(display, [255, 0, 0],
+    #                    [stone["coord_draw"][0] + size_stone[0] / 2, stone["coord_draw"][1] + size_stone[1] / 2], 3)
+    pygame.draw.rect(display, [255, 255, 255], [stone["coord"][0], stone["coord"][1], size_stone[0], size_stone[1]], 3)
 
 
 stone = pygame.image.load("pics/Meteorit.png")
